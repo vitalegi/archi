66fdcbef218a91950859a03e44da7aecb2500b80
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2024-06-18 18:15:34 +0200

    when loading workspace from files, workspace fields are not required

--- src/test/java/it/vitalegi/archi/ExampleTests.java
+++ src/test/java/it/vitalegi/archi/ExampleTests.java
@@ -1,9 +1,5 @@
 package it.vitalegi.archi;
 
-import it.vitalegi.archi.diagram.DiagramFormat;
-import it.vitalegi.archi.exporter.c4.plantuml.PlantumlDiagramExporter;
-import it.vitalegi.archi.model.builder.WorkspaceDirector;
-import it.vitalegi.archi.workspaceloader.FileSystemWorkspaceLoader;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -15,22 +11,14 @@ public class ExampleTests {
 
     @Test
     void simpleWebapp() {
-        build(Path.of("examples", "simple-webapp"));
+        var root = Path.of("examples", "simple-webapp");
+        App.execute(root, root.resolve("output"));
     }
 
     @Test
     void properties() {
-        build(Path.of("examples", "properties"));
-    }
+        var root = Path.of("examples", "properties");
+        App.execute(root, root.resolve("output"));
 
-    void build(Path baseDir) {
-        var out = baseDir.resolve("output");
-
-        var fsLoader = new FileSystemWorkspaceLoader();
-        var factory = new WorkspaceDirector();
-        factory.makeWorkspace(fsLoader.load(baseDir));
-        var workspace = factory.build();
-        var exporter = new PlantumlDiagramExporter(workspace, out, DiagramFormat.values());
-        workspace.getDiagrams().getAll().forEach(exporter::export);
     }
 }
