6d0dfd2be7dd4d6a92d889b236b3cae4a87efb09
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2024-06-23 16:24:45 +0200

    export of diagrams is done in parallel

--- src/main/java/it/vitalegi/archi/App.java
+++ src/main/java/it/vitalegi/archi/App.java
@@ -3,15 +3,15 @@ package it.vitalegi.archi;
 import it.vitalegi.archi.diagram.DiagramFormat;
 import it.vitalegi.archi.exporter.c4.plantuml.PlantumlDiagramExporter;
 import it.vitalegi.archi.model.builder.WorkspaceDirector;
+import it.vitalegi.archi.model.diagram.Diagram;
 import it.vitalegi.archi.workspaceloader.FileSystemWorkspaceLoader;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 
 import java.nio.file.Path;
+import java.util.List;
 
+@Slf4j
 public class App {
-
-    private static final Logger log = LoggerFactory.getLogger(App.class);
 
     public static void main(String[] args) throws Exception {
         if (args.length < 2) {
@@ -31,6 +31,18 @@ public class App {
         var workspace = factory.build();
         log.info("Loaded workspace");
         var exporter = new PlantumlDiagramExporter(workspace, outputDir, DiagramFormat.values());
-        workspace.getDiagrams().getAll().forEach(exporter::export);
+        export(exporter, workspace.getDiagrams().getAll());
+    }
+
+    protected static void export(PlantumlDiagramExporter exporter, List<Diagram> diagrams) {
+        diagrams.parallelStream().forEach(diagram -> export(exporter, diagram));
+    }
+
+    protected static void export(PlantumlDiagramExporter exporter, Diagram diagram) {
+        log.info("Export of {} started", diagram.getName());
+        var startTime = System.currentTimeMillis();
+        exporter.export(diagram);
+        var duration = System.currentTimeMillis() - startTime;
+        log.info("Export of {} done in {}ms", diagram.getName(), duration);
     }
 }
